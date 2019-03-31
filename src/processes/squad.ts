9c4a1d268b694fdbbb817d8fd1c8e57b3a53c16a
RvStFyth <phpgeweld@gmail.com>
2019-03-31 14:30:20 +0200

    started on squad code WIP

--- src/processes/squad.ts
+++ src/processes/squad.ts
@@ -1,4 +1,5 @@
 import { Process } from "ROS/process";
+import {MapHelper} from "../helpers/map";
 
 export class Squad extends Process
 {
@@ -44,26 +45,6 @@ export class Squad extends Process
         return this.meta.creeps.filter((c: string) => Game.creeps[c] && !Game.creeps[c].spawning && Game.creeps[c].pos.inRangeTo(cc, 5)).length === 4;
     }
 
-    private moveToFormation(creeps: Creep[])
-    {
-        let valid = true;
-        if(creeps[1].pos.x !== creeps[0].pos.x + 1 && creeps[1].pos.y !== creeps[0].pos.y) {
-            valid = false;
-            creeps[1].moveTo(creeps[0].pos.x + 1, creeps[0].pos.y);
-        }
-        if(creeps[2].pos.x !== creeps[0].pos.x && creeps[2].pos.y !== creeps[0].pos.y + 1) {
-            valid = false;
-            creeps[2].moveTo(creeps[0].pos.x, creeps[0].pos.y + 1);
-        }
-        if(creeps[3].pos.x !== creeps[0].pos.x + 1 && creeps[3].pos.y !== creeps[0].pos.y + 1) {
-            valid = false;
-            creeps[3].moveTo(creeps[0].pos.x + 1, creeps[0].pos.y + 1);
-        }
-        if(valid) {
-            this.meta.inFormation = true;
-        }
-    }
-
     /**
      * Formation:
      * 01
@@ -73,6 +54,41 @@ export class Squad extends Process
      */
     private handleCreeps(creeps: Creep[])
     {
+        if(!this.meta.route) {
+            const rooms: any = MapHelper.findRoute(this.meta.room, this.meta.target, {
+                ignoreSK: false, prioritizeHighways:true
+            });
+            if(rooms && rooms.length) {
+                this.meta.rooms = rooms;
+            }
+            else {
+                console.log(`FAILED TO FIND A ROUTE FROM ${this.meta.room} TO ${this.meta.target}`);
+            }
+        }
+        if(!this.meta.directions) {
+            // Calculate the path for in the current room.
+            // Store a string with direction integers. Slice the first each tick and use creep.move() with
+            // the resulting integer.
+        }
+    }
 
+    private moveToFormation(creeps: Creep[])
+    {
+        let valid = true;
+        if(creeps[1].pos.x !== creeps[0].pos.x + 1 && creeps[1].pos.y !== creeps[0].pos.y) {
+            valid = false;
+            creeps[1].moveTo(creeps[0].pos.x + 1, creeps[0].pos.y, {maxRooms: 1});
+        }
+        if(creeps[2].pos.x !== creeps[0].pos.x && creeps[2].pos.y !== creeps[0].pos.y + 1) {
+            valid = false;
+            creeps[2].moveTo(creeps[0].pos.x, creeps[0].pos.y + 1, {maxRooms: 1});
+        }
+        if(creeps[3].pos.x !== creeps[0].pos.x + 1 && creeps[3].pos.y !== creeps[0].pos.y + 1) {
+            valid = false;
+            creeps[3].moveTo(creeps[0].pos.x + 1, creeps[0].pos.y + 1, {maxRooms: 1});
+        }
+        if(valid) {
+            this.meta.inFormation = true;
+        }
     }
 }
